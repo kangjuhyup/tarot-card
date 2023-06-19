671533ca559759c708ce3d4461c5d4a42e5aef5d
mantisco-kangjuhyup <fog0510@gmail.com>
2023-06-19 22:13:35 +0900

    fetch to axios

--- client/src/hooks/authHooks.ts
+++ client/src/hooks/authHooks.ts
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { useState } from "react";
+import Cookies from "universal-cookie";
 import { authKakaoDto } from "./dto/authKakao.dto";
 
 const useAuth = () => {
@@ -6,30 +8,35 @@ const useAuth = () => {
     const [error, setError] = useState(null);
 
     const authKakao = async (data: authKakaoDto) => {
-        setLoading(true);
-        try {
-          const url = 
-          `${process.env.REACT_APP_TAROT_SERVER}/auth/kakao`;
-          const response = await fetch(url,{
-              headers : {
-                "Authoriztion" : `Bearer ${data.access_token}`
-              }
-          });
-          if (response.ok) {
-            const jsonData = await response.json();
-            return jsonData;
-          } else if (response.status === 613) {
-            return { success: false };
-          } else {
-            throw Error("failed call getPolicy API");
-          }
-        } catch (error:any) {
-          setError(error);
-          throw error;
-        } finally {
-          setLoading(false);
+      setLoading(true);
+      try {
+        const url = `${process.env.REACT_APP_TAROT_SERVER}/auth/kakao`;
+        const response = await axios.get(url, {
+          headers: {
+            Authorization: `Bearer ${data.access_token}`,
+          },
+        });
+    
+        if (response.status === 200) {
+          const jsonData = response.data;
+          const jwtToken = jsonData.access;
+          const cookies = new Cookies();
+          cookies.set("access", jwtToken, { path: "/" });
+          
+          return jsonData;
+        } else if (response.status === 613) {
+          return { success: false };
+        } else {
+          throw Error("failed call getPolicy API");
         }
+      } catch (error: any) {
+        setError(error);
+        throw error;
+      } finally {
+        setLoading(false);
       }
+    };
+    
       
       return {
           loading,
--- client/src/hooks/tarotHooks.ts
+++ client/src/hooks/tarotHooks.ts
@@ -1,39 +1,44 @@
 import { useState } from "react";
+import Cookies from "universal-cookie";
 import { authKakaoDto } from "./dto/authKakao.dto";
 import { getResultDto } from "./dto/getResult.dto";
 
 const useTarot = () => {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const getResult = async (data: getResultDto) => {
-        setLoading(true);
-        try {
-          const url = 
-          `${process.env.REACT_APP_TAROT_SERVER}/tarot/result?type_num=${data.type_num}&first_card_num=${data.first_card_num}&first_forward=${data.first_forward}&second_card_num=${data.second_card_num}&second_forward=${data.second_forward}&third_card_num=${data.third_card_num}&third_forward=${data.third_forward}`;
-          const response = await fetch(url);
-          if (response.ok) {
-            const jsonData = await response.json();
-            return jsonData;
-          } else if (response.status === 613) {
-            return { success: false };
-          } else {
-            throw Error("failed call getPolicy API");
-          }
-        } catch (error:any) {
-          setError(error);
-          throw error;
-        } finally {
-          setLoading(false);
-        }
-      };
-
-      
-      return {
-          loading,
-          error,
-          getResult
+  const getResult = async (data: getResultDto) => {
+    setLoading(true);
+    try {
+      const url = `${process.env.REACT_APP_TAROT_SERVER}/tarot/result?type_num=${data.type_num}&first_card_num=${data.first_card_num}&first_forward=${data.first_forward}&second_card_num=${data.second_card_num}&second_forward=${data.second_forward}&third_card_num=${data.third_card_num}&third_forward=${data.third_forward}`;
+      const cookies = new Cookies();
+      const jwt = cookies.get("access");
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      if (response.ok) {
+        const jsonData = await response.json();
+        return jsonData;
+      } else if (response.status === 613) {
+        return { success: false };
+      } else {
+        throw Error("failed call getPolicy API");
       }
-}
+    } catch (error: any) {
+      setError(error);
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    loading,
+    error,
+    getResult,
+  };
+};
 
 export default useTarot;
